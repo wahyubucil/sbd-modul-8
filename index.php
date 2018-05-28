634232b8d4b521f0ad88ddc8e18c97b8f3c7b541
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-05-28 15:49:59 +0800

    Create header

--- index.php
+++ index.php
@@ -5,11 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tabel Cuti</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <style>
-        
+        body {
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        header {
+            width: 100%;
+            text-align: center;
+            background: #304ffe;
+            color: #ffffff;
+            float: left;
+        }
     </style>
 </head>
 <body>
-    
+    <header>
+        <h1>TABEL CUTI</h1>
+    </header>
 </body>
 </html>
