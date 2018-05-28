199d2d36cf5f5aa968180de3afcf7b4bc792cd64
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-05-28 16:26:00 +0800

    Create table structure with dummy data

---
+++ table_select.php
@@ -0,0 +1,25 @@
+<table>
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>NIK</th>
+            <th>Tanggal Mulai</th>
+            <th>Lama Cuti</th>
+            <th>Keterangan</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+       <tr>
+           <td>1</td>
+           <td>20140012</td>
+           <td>2018-10-10</td>
+           <td>3 Hari</td>
+           <td>Sakit</td>
+           <td>
+               <a href="?page=edit&id=1">Edit</a>
+               <a href="?page=delete&id=1">Delete</a>
+           </td>
+       </tr> 
+    </tbody>
+</table>
